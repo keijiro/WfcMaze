8a7a2d1a792b3638da85de43f418a18a901fb5da
Keijiro Takahashi <keijiro@gmail.com>
2021-10-27 23:26:33 +0900

    Optimize BitField with Burst

--- Assets/Wfc/BitField.cs
+++ Assets/Wfc/BitField.cs
@@ -1,5 +1,10 @@
+using Unity.Burst;
+using Unity.Burst.Intrinsics;
+using static Unity.Burst.Intrinsics.Arm.Neon;
+
 namespace Wfc
 {
+    [BurstCompile]
     unsafe struct BitField
     {
         #region Public methods
@@ -24,21 +29,9 @@ namespace Wfc
             _fields[bits / 64] = (1ul << (bits % 64)) - 1ul;
         }
 
-        public int CountBits()
-        {
-            var count = 0;
-            for (var i = 0; i < Length; i++)
-                count += Util.CountBits(_fields[i]);
-            return count;
-        }
+        public int CountBits() => CountBits(this);
 
-        public int FindNthOne(int n)
-        {
-            var count = 0;
-            for (var i = 0; i < Length * 64; i++)
-                if (GetBit(i) && count++ == n) return i;
-            return -1;
-        }
+        public int FindNthOne(int n) => FindNthOne(this, n);
 
         #endregion
 
@@ -46,6 +39,38 @@ namespace Wfc
 
         const int Length = 4;
         fixed ulong _fields[Length];
+
+        #endregion
+
+        #region Burst functions
+
+        [BurstCompile]
+        static int CountBits(in BitField bf)
+        {
+            var count = 0;
+            for (var i = 0; i < Length; i++)
+                count += CountBitsUL(bf._fields[i]);
+            return count;
+        }
+
+        [BurstCompile]
+        static int FindNthOne(in BitField bf, int n)
+        {
+            var count = 0;
+            for (var i = 0; i < Length * 64; i++)
+                if (bf.GetBit(i) && count++ == n) return i;
+            return -1;
+        }
+
+        static int CountBitsUL(ulong x)
+        {
+            if (IsNeonSupported) return vaddv_u8(vcnt_u8(new v64(x)));
+
+            var count = 0;
+            for (var i = 0; i < 64; i++)
+                if (((1ul << i) & x) != 0) count++;
+            return count;
+        }
 
         #endregion
     }
